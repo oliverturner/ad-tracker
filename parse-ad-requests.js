64c516ba3b28a246f9fa186860e29feb535dc339
Oliver Turner <oliverturner@users.noreply.github.com>
2022-05-04 23:04:58 +0100

    Initial commit

---
+++ parse-ad-requests.js
@@ -0,0 +1,88 @@
+function interceptRequests() {
+  var XHR = XMLHttpRequest.prototype;
+  var send = XHR.send;
+  var open = XHR.open;
+  let counter = 0;
+
+  function processCustParams(cust_params) {
+    const custValues = new URLSearchParams(cust_params);
+    const {
+      device_spoor_id,
+      guid,
+      loggedIn,
+      permutive,
+      "permutive-id": permutiveId,
+      ...other
+    } = Object.fromEntries(custValues);
+
+    return {
+      device_spoor_id,
+      guid,
+      loggedIn,
+      permutive,
+      permutiveId,
+      other,
+    };
+  }
+
+  function onLoad() {
+    if (
+      this.url?.includes("https://securepubads.g.doubleclick.net/gampad/ads")
+    ) {
+      const reqUrl = new URL(this.url);
+      const allParams = Object.fromEntries(reqUrl.searchParams);
+
+      const {
+        cookie,
+        correlator,
+        prev_iu_szs,
+        prev_scp,
+        us_privacy,
+        cust_params,
+        ...other
+      } = allParams;
+
+      const data = {
+        prev_iu_szs,
+        prev_scp,
+        us_privacy,
+        cust_params: processCustParams(cust_params),
+        other,
+      };
+
+      console.log(`ad-${++counter}`, data);
+    }
+  }
+
+  /**
+   * @param {string} _method
+   * @param {*} url
+   * @returns
+   */
+  XHR.open = function (_method, url) {
+    this.url = url; // the request url
+    return open.apply(this, arguments);
+  };
+
+  XHR.send = function () {
+    this.addEventListener("load", onLoad);
+    return send.apply(this, arguments);
+  };
+}
+
+function interceptData() {
+  var xhrOverrideScript = document.createElement("script");
+  xhrOverrideScript.type = "text/javascript";
+  xhrOverrideScript.innerHTML = `(${interceptRequests})();`;
+  document.head.prepend(xhrOverrideScript);
+}
+
+function checkForDOM() {
+  if (document.body && document.head) {
+    interceptData();
+  } else {
+    requestIdleCallback(checkForDOM);
+  }
+}
+
+requestIdleCallback(checkForDOM);
