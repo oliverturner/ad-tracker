3ef85dae683197a72fd363a250acb27d2b0d01bc
Oliver Turner <oliverturner@users.noreply.github.com>
2022-05-05 10:55:20 +0100

    Rebuild script

--- dist/ad-tracker.js
+++ dist/ad-tracker.js
@@ -160,15 +160,19 @@
     const interceptScript = document.createElement("script");
     interceptScript.innerHTML = `
     // Expose the 'inspectSlots' function
+    //-----------------------------------------------------------------------
     ${inspectSlots}
 
     // Immediately overwrite the XMLHttpRequest object
+    //-----------------------------------------------------------------------
     (${interceptRequests})()
   `;
 
     document.head.prepend(interceptScript);
   }
 
+  // Poll the DOM to see if it's ready
+  //-----------------------------------------------------------------------
   function checkForDOM() {
     document.body && document.head
       ? appendScripts()
